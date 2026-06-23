c6b74be4503d163beafcd3df2eba9f8361853502
RashidMahmud <rashidmahmud999@gmail.com>
2026-06-23 14:17:45 +0600

    set up environment configuration and initialize Express app with CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import app from "./app";
-import { prisma } from "./lib/prisma";
+import config from "./config";
+import "dotenv/config";
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.port;
 
 async function main() {
   try {
-    await prisma.$connect();
+    // await prisma.$connect();
     console.log("Connected to the database successfully.");
     app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting the server:", error);
-    await prisma.$disconnect();
+    // await prisma.$disconnect();
     process.exit(1);
   }
 }
