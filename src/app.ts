147e4fa8d04bc513c524b609836a1887354ebaf1
RashidMahmud <rashidmahmud999@gmail.com>
2026-06-23 22:25:15 +0600

    refactor user registration endpoint to include profile creation and return user data without password

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
@@ -33,9 +32,39 @@ app.post("/api/users/register", async (req: Request, res: Response) => {
     password,
     Number(config.bcryptSaltRounds),
   );
-  res
-    .status(httpStatus.CREATED)
-    .json({ message: "User registered successfully" });
+  const CreatedUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  await prisma.profile.create({
+    data: {
+      userId: CreatedUser.id,
+      profilePhoto,
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: CreatedUser.id,
+      email: CreatedUser.email || email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statuscode: httpStatus.CREATED,
+    message: "User registered successfully",
+    data: {
+      user,
+    },
+  });
 });
 
 export default app;
