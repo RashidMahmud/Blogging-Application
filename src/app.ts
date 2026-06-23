e3ffddd730faa674466819f2267d7f8e48f31f5a
RashidMahmud <rashidmahmud999@gmail.com>
2026-06-23 21:48:10 +0600

    add user registration endpoint with email uniqueness check and password hashing

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
-
+import prisma from "./lib/prisma";
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 const app: Application = express();
 
 app.use(
@@ -17,6 +19,23 @@ app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
+});
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const { name, email, password, profilePhoto } = req.body;
+
+  const isUserExists = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (isUserExists) {
+    throw new Error("User already exists");
+  }
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcryptSaltRounds),
+  );
+  res
+    .status(httpStatus.CREATED)
+    .json({ message: "User registered successfully" });
 });
 
 export default app;
