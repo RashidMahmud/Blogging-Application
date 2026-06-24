f072a4547fe98c140de2e1b4811e5ad8ba457d79
RashidMahmud <rashidmahmud999@gmail.com>
2026-06-25 00:56:09 +0600

    profile added in CreateUser

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -20,14 +20,19 @@ const registerUserIntoDB = async (payload: RegisterUserPayload) => {
       name,
       email,
       password: hashedPassword,
+      profile: {
+        create: {
+          profilePhoto,
+      }
+      }
     },
   });
-  await prisma.profile.create({
-    data: {
-      userId: CreatedUser.id,
-      profilePhoto,
-    },
-  });
+  // await prisma.profile.create({
+  //   data: {
+  //     userId: CreatedUser.id,
+  //     profilePhoto,
+  //   },
+  // });
   const user = await prisma.user.findUnique({
     where: {
       id: CreatedUser.id,
