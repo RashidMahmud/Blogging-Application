fdf42de5a6f99c0ae751c11f1a389cd88ad69e15
RashidMahmud <rashidmahmud999@gmail.com>
2026-06-25 02:54:08 +0600

    userController clean code

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,8 +2,7 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { UserService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
-
-
+import { sendResponse } from "../../utils/sendResponse";
 
 // const registerUser = async (req: Request, res: Response) => {
 //   try {
@@ -28,18 +27,26 @@ import { catchAsync } from "../../utils/catchAsync";
 //     });
 //   }
 // };
+
+
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const user = await UserService.registerUserIntoDB(payload);
-    res.status(httpStatus.CREATED).json({
+    // res.status(httpStatus.CREATED).json({
+    //   success: true,
+    //   statuscode: httpStatus.CREATED,
+    //   message: "User registered successfully",
+    //   data: {
+    //     user,
+    //   },
+    // });
+    sendResponse(res, {
       success: true,
       statuscode: httpStatus.CREATED,
       message: "User registered successfully",
-      data: {
-        user,
-      },
-    });
+      data: { user }
+    })
   },
 );
 
