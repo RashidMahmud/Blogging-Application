2cd24718642c042a20ea5b53c2e706f0758c31d3
RashidMahmud <rashidmahmud999@gmail.com>
2026-06-24 13:38:05 +0600

    implement user registration endpoint with email check and password hashing

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,7 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
-import prisma from "./lib/prisma";
-import httpStatus from "http-status";
-import bcrypt from "bcryptjs";
+import { userRoutes } from "./modules/user/user.route";
 const app: Application = express();
 
 app.use(
@@ -19,52 +17,7 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
-app.post("/api/users/register", async (req: Request, res: Response) => {
-  const { name, email, password, profilePhoto } = req.body;
-
-  const isUserExists = await prisma.user.findUnique({
-    where: { email },
-  });
-  if (isUserExists) {
-    throw new Error("User already exists");
-  }
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcryptSaltRounds),
-  );
-  const CreatedUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-  await prisma.profile.create({
-    data: {
-      userId: CreatedUser.id,
-      profilePhoto,
-    },
-  });
-  const user = await prisma.user.findUnique({
-    where: {
-      id: CreatedUser.id,
-      email: CreatedUser.email || email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statuscode: httpStatus.CREATED,
-    message: "User registered successfully",
-    data: {
-      user,
-    },
-  });
-});
+// app.post();
+app.use("/api/user", userRoutes);
 
 export default app;
