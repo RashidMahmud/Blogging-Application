f9a401e862ab1f6a202732450b0a9b2a2d44ebb2
RashidMahmud <rashidmahmud999@gmail.com>
2026-07-02 23:22:18 +0600

    add token verification to getMyProfile function

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,8 @@ import httpStatus from "http-status";
 import { UserService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import jwt from "jsonwebtoken";
+import config from "../../config";
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -29,6 +31,10 @@ const getMyProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { accessToken } = req.cookies;
     console.log(accessToken);
+    const verifiedToken = jwt.verify(accessToken, config.jwt_access_secret);
+
+    console.log(verifiedToken);
+
     res.send("Get My Profile");
   },
 );
