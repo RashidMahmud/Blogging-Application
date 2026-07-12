adad81f830765e8527ffe50b980b0b775c455769
RashidMahmud <rashidmahmud999@gmail.com>
2026-07-13 00:01:48 +0600

    feat: implement update profile functionality in user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -55,7 +55,11 @@ const getMyProfile = catchAsync(
 );
 
 const updateMyProfile = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {})
+  async (req: Request, res: Response, next: NextFunction) => {
+    const usrId = req.user?.id as string;
+    const payload = req.body;
+    const updatedProfile = await UserService.updateMyProfileInDB(usrId, payload);
+  })
     
 
 export const userController = {
