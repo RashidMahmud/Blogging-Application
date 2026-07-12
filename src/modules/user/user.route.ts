21505a34cd64ec3f7ecc03eaf0a4fc524b738f08
RashidMahmud <rashidmahmud999@gmail.com>
2026-07-12 23:41:34 +0600

    refactor: streamline token extraction logic in auth middleware

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -25,10 +25,11 @@ const router = Router();
 router.post("/register", userController.registerUser);
 const auth = (...requiredRoles: Role[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.accessToken;
-    // || req.headers.authorization?.startsWith("Bearer ")
-    //   ? req.headers.authorization?.split(" ")[1]
-    //   : req.headers.authorization;
+    const token = req.cookies.accessToken
+      ? req.cookies.accessToken
+      : req.headers.authorization?.startsWith("Bearer ")
+        ? req.headers.authorization?.split(" ")[1]
+        : req.headers.authorization;
 
     if (!token) {
       throw new Error(
