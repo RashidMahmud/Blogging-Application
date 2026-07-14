b54c191bd94b1e6bb09a5fa020af60d9b883d032
RashidMahmud <rashidmahmud999@gmail.com>
2026-07-14 20:54:41 +0600

    feat: implement refresh token verification in auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -50,7 +50,24 @@ const loginUser = async (payload: ILoginUser) => {
   };
 };
 
-const refreshToken = async (refreshToken: string) => {};
+const refreshToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = jwtUtils.verifyToken(
+    refreshToken,
+    config.jwt_refresh_secret,
+  );
+
+  if (!verifiedRefreshToken.success) {
+    throw new Error(verifiedRefreshToken.message);
+  }
+  const {id} = verifiedRefreshToken;
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {id}
+  })
+  if(user.activeStatus === "BLOCKED") {
+    throw new Error("User is blocked!");
+  }
+};
 
 export const authService = {
   loginUser,
